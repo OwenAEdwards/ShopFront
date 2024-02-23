c6e297025961638cd0c7c6b39f9942a639168881
grabercn <cgrab713@gmail.com>
2024-02-23 13:10:15 -0500

    add basic boiler code for components, render all components in Home.js

--- Application (Implementation)/dbd-site/src/components/Home.js
+++ Application (Implementation)/dbd-site/src/components/Home.js
@@ -1,13 +1,27 @@
-// Home.js
-import React from 'react';
+import React, { useState } from 'react';
+import Login from './Auth/Login';
+import Signup from './Auth/Signup';
+import Cart from './Cart';
+import ProductDetail from './ProductDetail';
+import ProductList from './ProductList';
 
 const Home = () => {
-  return (
-    <div>
-      <h1>Welcome to Our E-Commerce Store!</h1>
-      {/* Add featured products, categories, and promotions */}
-    </div>
-  );
-};
+    const [selectedProduct, setSelectedProduct] = useState(null);
+
+    const handleProductClick = (product) => {
+        setSelectedProduct(product);
+    };
+
+    return (
+        <div>
+            <h1>Welcome to Our E-commerce Site</h1>
+            <Login />
+            <Signup />
+            <Cart />
+            <ProductList onProductClick={handleProductClick} />
+            {selectedProduct && <ProductDetail product={selectedProduct} />}
+        </div>
+    );
+}
 
 export default Home;
