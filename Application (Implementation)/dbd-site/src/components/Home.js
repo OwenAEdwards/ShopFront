db45aec3e36bb6706b5ee94f2a9bc138d7eaffb5
grabercn <cgrab713@gmail.com>
2024-02-23 12:56:45 -0500

    initial skeleton, added basic homepage, setup react router, added component files, fixed outdated react errors

---
+++ Application (Implementation)/dbd-site/src/components/Home.js
@@ -0,0 +1,13 @@
+// Home.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Our E-Commerce Store!</h1>
+      {/* Add featured products, categories, and promotions */}
+    </div>
+  );
+};
+
+export default Home;
