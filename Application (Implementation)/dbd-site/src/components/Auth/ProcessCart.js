6a1cfe3e4486facf21ff66d7c3b814f7ccffaadd
grabercn <cgrab713@gmail.com>
2024-04-04 18:04:30 -0400

    process cart infra

---
+++ Application (Implementation)/dbd-site/src/components/Auth/ProcessCart.js
@@ -0,0 +1,9 @@
+.// ProcessCart.js
+// ProcessCart is a page that processes the cart of a user.
+import React from 'react';
+
+const ProcessCart = () => {
+    return <h1>ProcessCart is here</h1>;
+}
+
+export default ProcessCart;
