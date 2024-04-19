7b62b36ebb2ed9c47fcbabac2ff1c270848e5139
grabercn <cgrab713@gmail.com>
2024-04-18 20:42:42 -0400

    container

--- Application (Implementation)/dbd-site/src/components/Auth/UserProfile.js
+++ Application (Implementation)/dbd-site/src/components/Auth/UserProfile.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { checkName, checkAuth, checkCookie } from '../Helpers/auth';
+import { Container } from '@mui/material';
 
 const UserProfile = () => {
     const name = checkName();
 
     return (
         <div>
+            <Container>
             <h1>Hello {name}</h1>
             <p>This is the user profile page</p>
             <p>You are currently logged in as a {checkAuth()}</p>
             <p>Is the user logged in? {checkCookie() ? 'Yes' : 'No'}</p>
             <p>Balance: NEEDS IMPLEMENTED</p>
+            </Container>
         </div>
     );
 };
