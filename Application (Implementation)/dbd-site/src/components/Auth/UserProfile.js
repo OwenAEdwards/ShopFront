01f00ae58b842d5ad3cfec12b65f0540bb822fa0
grabercn <cgrab713@gmail.com>
2024-04-20 17:31:47 -0400

    removed bal

--- Application (Implementation)/dbd-site/src/components/Auth/UserProfile.js
+++ Application (Implementation)/dbd-site/src/components/Auth/UserProfile.js
@@ -12,7 +12,6 @@ const UserProfile = () => {
             <p>This is the user profile page</p>
             <p>You are currently logged in as a {checkAuth()}</p>
             <p>Is the user logged in? {checkCookie() ? 'Yes' : 'No'}</p>
-            <p>Balance: NEEDS IMPLEMENTED</p>
             </Container>
         </div>
     );
