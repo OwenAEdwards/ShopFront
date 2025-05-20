49f6190dffdab3b569bda105e5047a7d43ac4565
grabercn <cgrab713@gmail.com>
2025-05-20 18:11:35 -0400

    added 404 button as pointer and use router instead of http redirec

--- src/frontend/src/pages/NotFoundPage.tsx
+++ src/frontend/src/pages/NotFoundPage.tsx
@@ -1,15 +1,18 @@
 import { Button } from "@/components/ui/button"
+import { useNavigate } from "react-router-dom"
 
 function NotFoundPage() {
-  return (
-  <div className="flex flex-col items-center justify-center h-screen text-red-500">
-    <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
-    <p className="mb-8">Sorry, the page you are looking for does not exist.</p>
-    <Button variant="default" onClick={() => window.location.href = '/'}>
-      Go to Home
-    </Button>
-  </div>
-  )
+    const navigate = useNavigate()
+
+    return (
+    <div className="flex flex-col items-center justify-center h-screen text-red-500">
+        <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
+        <p className="mb-8">Sorry, the page you are looking for does not exist.</p>
+        <Button className="cursor-pointer" variant="default" onClick={() => navigate("/")}>
+        Go to Home
+        </Button>
+    </div>
+    )
 }
 
 export default NotFoundPage
